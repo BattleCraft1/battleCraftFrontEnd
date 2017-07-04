c8f46880a02f71b27e56ce76915dd52fa762a7d6
tomaszblawucki <tomasz.blawucki@pollub.edu.pl>
2017-07-04 21:42:02 +0200

    New project tree

---
+++ src/components/navbar/Navbar.css.js
@@ -0,0 +1,14 @@
+export default {
+logo:{
+    fontFamily:"LogoFont",
+    fontSize: "20px",
+},
+navbar:{
+    backgroundImage: "url('brown-texture.jpg')",
+    borderRadius:"0",
+    marginTop:"30px",
+    zIndex: "999",
+    fontSize: "15px",
+}
+}
+        
